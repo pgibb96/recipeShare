5bf5fddfd968a8079264406c059cb58d79fe4503
pgibb96 <34801002+pgibb96@users.noreply.github.com>
2022-04-20 23:17:46 -0400

    backend

---
+++ server/main.go
@@ -0,0 +1,34 @@
+package main
+
+import (
+	"os"
+
+	"github.com/pgibb96/recipeShare/server/routes"
+
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	port := os.Getenv("PORT")
+
+	if port == "" {
+		port = "8000"
+	}
+
+	router := gin.New()
+	router.Use(gin.Logger())
+
+	router.Use(cors.Default())
+
+	// these are the endpoints
+	//C
+	router.POST("/user/create", routes.AddUser)
+	//R
+	//U
+
+	//D
+
+	//this runs the server and allows it to listen to requests.
+	router.Run(":" + port)
+}
---
+++ server/routes/users.go
@@ -0,0 +1,52 @@
+package routes
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/pgibb96/recipeShare/server/models"
+
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator/v10"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+)
+
+var validate = validator.New()
+
+var orderCollection *mongo.Collection = OpenCollection(Client, "users")
+
+// add a user
+func AddUser(c *gin.Context) {
+
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+	var user models.User
+
+	if err := c.BindJSON(&user); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		fmt.Println(err)
+		return
+	}
+
+	validationErr := validate.Struct(user)
+	if validationErr != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": validationErr.Error()})
+		fmt.Println(validationErr)
+		return
+	}
+	user.ID = primitive.NewObjectID()
+
+	result, insertErr := orderCollection.InsertOne(ctx, user)
+	if insertErr != nil {
+		msg := fmt.Sprintf("user was not created")
+		c.JSON(http.StatusInternalServerError, gin.H{"error": msg})
+		fmt.Println(insertErr)
+		return
+	}
+	defer cancel()
+
+	c.JSON(http.StatusOK, result)
+}
